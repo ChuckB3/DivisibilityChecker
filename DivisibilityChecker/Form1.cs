d5eade75cd31fd50e0268b0617436e1bf3b28035
ChuckB3 <cbrumbaugh3@gmail.com>
2017-04-20 13:19:58 -0400

    importing project

---
+++ DivisibilityChecker/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DivisibilityChecker
+{
+    public partial class DivisibilityChecker : Form
+    {
+        public List<String> results = new List<String>();        
+        public int divBy;
+
+        public DivisibilityChecker()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            results.Clear();
+            divBy = Int32.Parse(divByBox.Text);
+
+            for(int i = 1; i <= 100; i++)
+            {
+                if (i%divBy==0)
+                {
+                    results.Add(i.ToString());
+                }
+            }
+            var message = string.Join(Environment.NewLine, results.ToArray());
+            MessageBox.Show(message, "Results");
+
+        }
+    }
+}
